93599feca58f8bc1771ae5dd5702fe903a0d4b37
Fajar Alfath <mfalfath25@gmail.com>
2023-10-22 15:46:55 +0700

    style: center loader

--- src/app/rankings/loading.tsx
+++ src/app/rankings/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react"
 
 export default function Loading() {
   return (
-    <div className="align-center flex h-screen items-center justify-center">
+    <div className="align-center flex h-screen items-center justify-center mx-auto">
       <Loader
         color="white"
         size={24}
